78885e6a97a74e7631a2a31e78a2d41e2484b63f
s23it163 <s23it163@akash.com>
2025-01-06 15:09:41 +0530

    Commmit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import Counter from './counter';
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+            <Counter/>
       </header>
     </div>
   );
---
+++ src/ToggleSwitch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToggleSwitch() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ToggleSwitch
+
