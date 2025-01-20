91faafccf39997ad6917b1739c5f5df8648e54b4
jayganjawala <jayganjawala.it22@scet.ac.in>
2025-01-20 15:11:34 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import logo from './logo.svg';
-import Areas from './Areas';
+import { Route, Link, Routes } from 'react-router-dom';
+
+import Home from './components/Home'
+import Contact from './components/contact'
+import About from './components/about'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-            <Areas/>
-      </header>
-    </div>
-  );
+
+    <div>
+            {/* Routes without using Switch */}
+                  <Routes>
+                          <Route path="/" exact element={<Home />} />
+                                  <Route path="/about" element={<About />} />
+                                          <Route path="/contact" element={<Contact />} />
+                                            
+                                                        </Routes>
+
+                                                              {/* Navigation links */}
+                                                                    <nav>
+                                                                            <ul>
+                                                                                      <li><Link to="/">Home</Link></li>
+                                                                                                <li><Link to="/about">About</Link></li>
+                                                                                                          <li><Link to="/contact">Contact</Link></li>
+                                                                                                                  
+                                                                                                                            </ul>
+                                                                                                                                  </nav>
+
+                                                                                                                                      </div>
+  )
+
 }
 
 export default App;
